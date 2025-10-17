07c9dabd09953c20088545678faa9bf201be72bc
Joseph Qian <jqian507@gmail.com>
2025-10-17 16:50:33 -0700

    finished gcd helper

--- CertificableEuclideanAlgorithm/CertifiableEuclideanAlgorithm.lean
+++ CertificableEuclideanAlgorithm/CertifiableEuclideanAlgorithm.lean
@@ -1,23 +1,33 @@
 import Lean
+import Mathlib.Data.Nat.Basic
 
-open Lean
+open Lean Meta
+open Nat
 
-def ExterndedEuclideanAlgorithm (a b : ℕ) : (ℕ × ℕ × ℕ) :=
+def ExterndedEuclideanAlgorithm (a b : ℕ) : MetaM (ℕ × ℕ × ℕ) := do
   -- Calculate gcd(a, b)
-  gcd (a b : ℕ) (d : ℤ) := do
-    r := a % b
-    if r = 0 then
-      pure b
+  let mut values := #[]
+  let rec
+  gcd (a b : ℕ) := do
+    let r := a % b
+    let mut out := 0
+    if b = 0 then
+      out := a
     else
-      Nat.gcd
+      out ← (gcd b r)
+    pure out
+  termination_by -- ???
 
-
+  let mut d := 0
   if a > b then
-    d ← gcd(a b)
+    d ← gcd a b
   else
-    d ← gcd(b a)
+    d ← gcd b a
 
   -- Back substitute to find x, y s.t. ax + by = d
+  let x := a
+  let y := b
+  pure (x, y, d)
 
 
 -- Checker: check that d|a, d|b, ax + by = d
